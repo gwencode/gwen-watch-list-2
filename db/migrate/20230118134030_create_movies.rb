bbc843df3100d6779807b92218e1db802af584a6
Gwendal Le Bris <gwen.coding@gmail.com>
2023-01-18 14:55:18 +0100

    Create models, associations, validations

---
+++ db/migrate/20230118134030_create_movies.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Class: CreateMovies
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :overview
+      t.string :poster_url
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
