48cab43fb6acdbf8bd55e41ac399d3f6799ab99f
Gwendal Le Bris <gwen.coding@gmail.com>
2023-01-31 16:38:25 +0100

    Add pg_search and style search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,9 @@ class Movie < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+                  against: :title,
+                  using: { tsearch: { prefix: true }, trigram: { word_similarity: true } }
 end
