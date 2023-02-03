b236900414d131ce541359b87c8abcf998dcd771
Gwendal Le Bris <gwen.coding@gmail.com>
2023-02-03 08:58:59 +0100

    Add Recommanded Movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,7 @@ class MoviesController < ApplicationController
   end
 
   def show
+    @reco_movies = @movie.reco_movies
   end
 
   private
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 require 'action_view'
+require 'json'
+require 'open-uri'
 
 class Movie < ApplicationRecord
   has_many :bookmarks
@@ -7,7 +9,6 @@ class Movie < ApplicationRecord
   has_many :actors, through: :casts
 
   validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true
 
   include PgSearch::Model
   pg_search_scope :search_by_title,
@@ -21,5 +22,60 @@ class Movie < ApplicationRecord
 
   def formatted_revenue
     number_with_delimiter(revenue, delimiter: ',')
+  end
+
+  def reco_movies
+    reco_movies = parsing_reco_movies(self)
+    five_reco_movies = []
+    reco_movies.each_with_index do |movie, index|
+      next if index > 4
+
+      if Movie.find_by(api_id: movie['id']).nil?
+        new_movie = Movie.create(
+          title: movie['title'],
+          overview: movie['overview'],
+          poster_url: movie['poster_path'].nil? ? '' : "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+          backdrop_url: movie['backdrop_path'].nil? ? '' : "https://image.tmdb.org/t/p/w1280#{movie['backdrop_path']}",
+          release_date: movie['release_date'],
+          api_id: movie['id']
+        )
+        adding_details(new_movie)
+        adding_director(new_movie)
+        five_reco_movies << new_movie
+      else
+        five_reco_movies << Movie.find_by(api_id: movie['id'])
+      end
+    end
+    five_reco_movies
+  end
+
+  private
+
+  def parsing_reco_movies(movie)
+    api_key = ENV['API_KEY']
+    movie_api_id = movie.api_id
+    url = "https://api.themoviedb.org/3/movie/#{movie_api_id}/recommendations?api_key=#{api_key}&language=en-US&page=1"
+    reco_movies_serialized = URI.open(url).read
+    JSON.parse(reco_movies_serialized)['results']
+  end
+
+  def adding_details(movie)
+    api_key = ENV['API_KEY']
+    url_movie = "https://api.themoviedb.org/3/movie/#{movie[:api_id]}?api_key=#{api_key}&language=en-US"
+    movie_details_serialized = URI.open(url_movie).read
+    movie_details = JSON.parse(movie_details_serialized)
+    movie.update(run_time: movie_details['runtime'],
+                 budget: movie_details['budget'],
+                 revenue: movie_details['revenue'],
+                 rating: movie_details['vote_average'])
+  end
+
+  def adding_director(movie)
+    api_key = ENV['API_KEY']
+    url_credits = "https://api.themoviedb.org/3/movie/#{movie[:api_id]}/credits?api_key=#{api_key}&language=en-US"
+    credits_serialized = URI.open(url_credits).read
+    credits = JSON.parse(credits_serialized)
+    director = credits['crew'].find { |crew| crew['job'] == 'Director' }
+    movie.update(director: director['name'])
   end
 end
