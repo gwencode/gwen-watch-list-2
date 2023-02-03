aaf3864ff9824607ae83389ee8e5790988301cdf
Gwendal Le Bris <gwen.coding@gmail.com>
2023-02-03 12:16:04 +0100

    Style Show list

--- db/migrate/20230118134421_create_bookmarks.rb
+++ db/migrate/20230118134421_create_bookmarks.rb
@@ -4,7 +4,6 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-      t.string :comment
       t.references :movie, null: false, foreign_key: true
       t.references :list, null: false, foreign_key: true
 
