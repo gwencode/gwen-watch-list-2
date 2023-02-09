1f572809330ce27e5b5daa96362d0973909fde3c
gwendal le bris <gwen.coding@gmail.com>
2023-02-09 16:22:19 +0100

    Add names to user, user profile view, user edit form. Still need to update user with first and last name in devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'my_movies/index'
   devise_for :users
+  get "user", to: "users#show"
   # get 'actors/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -16,5 +15,4 @@ Rails.application.routes.draw do
   resources :actors, only: %i[index show]
   resources :lists, only: %i[index show create destroy update]
   resources :my_movies, only: %i[index]
-  resources :users, only: %i[show]
 end
