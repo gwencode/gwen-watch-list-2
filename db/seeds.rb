435b1d12d47a9644821fdccb937d776937c3187d
gwendal le bris <gwen.coding@gmail.com>
2023-02-10 17:33:43 +0100

    Add a second user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,27 +86,33 @@ end
 def create_users_lists_bookmarks
   puts 'Creating users...'
   gwen = User.create(email: 'gwen@me.com', password: 'password')
-  puts '1 User created!'
+  john = User.create(email: 'john@me.com', password: 'password')
+  puts "#{User.count} users created!"
 
   puts 'Creating lists...'
   fantastic = List.create(name: 'Fantastic', user_id: gwen.id)
   horror = List.create(name: 'Horror', user_id: gwen.id)
   action = List.create(name: 'Action', user_id: gwen.id)
+  superhero = List.create(name: 'Superhero', user_id: john.id)
   puts "#{List.count} lists created!"
 
   puts 'Creating bookmarks...'
 
   Bookmark.create(movie: Movie.find_by(title: 'Avatar'), list: fantastic)
   Bookmark.create(movie: Movie.find_by(title: 'Avatar: The Way of Water'), list: fantastic)
-  Bookmark.create(movie: Movie.find_by(title: 'The Batman'), list: fantastic)
-  puts '3 movies added to fantastic list!'
+  Bookmark.create(movie: Movie.find_by(title: 'Black Adam'), list: fantastic)
+  puts '3 movies added to fantastic list of Gwendal!'
 
   Bookmark.create(movie: Movie.find_by(title: 'Violent Night'), list: horror)
   Bookmark.create(movie: Movie.find_by(title: 'M3GAN'), list: horror)
-  puts '2 movies added to horror list!'
+  puts '2 movies added to horror list of Gwendal!'
 
   Bookmark.create(movie: Movie.find_by(title: 'The Enforcer'), list: action)
-  puts '1 movie added to action list!'
+  puts '1 movie added to action list of Gwendal!'
+
+  Bookmark.create(movie: Movie.find_by(title: 'Black Panther: Wakanda Forever'), list: superhero)
+  Bookmark.create(movie: Movie.find_by(title: 'Black Adam'), list: superhero)
+  puts '2 movies added to superhero list of John!'
 end
 
 def parse_actors_casts
