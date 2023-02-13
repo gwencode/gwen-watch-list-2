4dc67182c712fa4be53b51f8ed91ac1240ad67aa
gwendal le bris <gwen.coding@gmail.com>
2023-02-13 12:22:52 +0100

    change list model: uniqueness scope

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :user_id }
 end
