07c3b4afdda9857be473b375494c4337daee8a54
gwendal le bris <gwen.coding@gmail.com>
2023-02-16 19:46:29 +0100

    Change redirection after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :store_location!, if: :storable_location?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -8,5 +9,15 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_location!
+    store_location_for(:user, request.fullpath)
   end
 end
