fe491082e847a7191eb8e9de997d238e3c9e1a6e
gwendal le bris <gwen.coding@gmail.com>
2023-02-19 19:03:53 +0100

    Config to deploy on render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.secret_key_base = 'a0aa8fbb9e700f0de9d68c2cece7749dc7c4db31628651eac64fd9020642dce103509196fb76a086c68bd6885a3cafa3d21d3f713ca9bca5dfb1907e5a31a9f0'
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
