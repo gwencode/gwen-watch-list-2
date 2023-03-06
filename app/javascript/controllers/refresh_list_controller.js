9d7bc23ff47b2a9ad731ec49d9e69c5da7a87401
gwendal le bris <gwen.coding@gmail.com>
2023-03-06 14:44:09 +0100

    Try to fix JS

--- app/javascript/controllers/refresh_list_controller.js
+++ app/javascript/controllers/refresh_list_controller.js
@@ -1,28 +1,28 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// // Connects to data-controller="refresh-list"
-// export default class extends Controller {
-//   static targets = [ "form", "list", "searchInput" ]
+// Connects to data-controller="refresh-list"
+export default class extends Controller {
+  static targets = [ "form", "list", "searchInput" ]
 
-//   connect() {
-//     // console.log("controller connected")
-//     // console.log("this.element = ", this.element)
-//     // console.log("this.formTarget = ", this.formTarget)
-//     // console.log("this.listTarget = ", this.listTarget)
-//     // console.log("this.searchInputTarget = ", this.searchInputTarget)
-//   }
+  connect() {
+    console.log("controller refresh list connected")
+    // console.log("this.element = ", this.element)
+    // console.log("this.formTarget = ", this.formTarget)
+    // console.log("this.listTarget = ", this.listTarget)
+    // console.log("this.searchInputTarget = ", this.searchInputTarget)
+  }
 
-//   update() {
-//     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
-//     // console.log("url = ", url)
-//     fetch(url, {
-//       headers: { 'Accept': 'application/json' }
-//       // body: new FormData(this.listTarget)
-//     })
-//       .then(response => response.json())
-//       .then((data) => {
-//         // console.log("data list = ", data.list)
-//         this.listTarget.outerHTML = data.list;
-//       })
-//   }
-// }
+  // update() {
+  //   const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
+  //   // console.log("url = ", url)
+  //   fetch(url, {
+  //     headers: { 'Accept': 'application/json' }
+  //     // body: new FormData(this.listTarget)
+  //   })
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       // console.log("data list = ", data.list)
+  //       this.listTarget.outerHTML = data.list;
+  //     })
+  // }
+}
